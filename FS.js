92ba792453b32b52836ccd1ef4c37380ea2edf3c
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-17 12:54:52 +0500

    Git Init

--- FS.js
+++ FS.js
@@ -1,5 +1,10 @@
 const fs = require('node:fs');
 
-fs.readFile('file.txt', 'utf8', (err, data) => {
-    console.log(err, data);
-});
+// fs.readFile('file.txt', 'utf8', (err, data) => {
+//     console.log(err, data);
+// });
+
+const sync = fs.readFileSync('file.txt');
+console.log(sync.toString())
+
+console.log("Finish Reading File");
