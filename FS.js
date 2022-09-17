09fdf9ea18cbb1aa7e2253f0dc274419115cb22d
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-17 13:09:51 +0500

    Git Init

--- FS.js
+++ FS.js
@@ -1,10 +1,17 @@
 const fs = require('node:fs');
 
-// fs.readFile('file.txt', 'utf8', (err, data) => {
+// fs.writeFile('File.txt', 'Learning Back-End With Node.JS!', () => {
+//     console.log("Written to the File");
+// });
+
+// fs.readFile('File.txt', 'utf8', (err, data) => {
 //     console.log(err, data);
 // });
 
-const sync = fs.readFileSync('file.txt');
-console.log(sync.toString())
+const write = fs.writeFileSync('File.txt', "Learning Back-End With Node.JS");
+console.log(write);
+
+const read = fs.readFileSync('File.txt');
+console.log(read.toString());
 
 console.log("Finish Reading File");
