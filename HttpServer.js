356eccc2c3b992de51812bdb48f1cd01799663af
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-17 19:23:22 +0500

    Git Init

---
+++ HttpServer.js
@@ -0,0 +1,5 @@
+const http = require("./node:event");
+
+const port = process.env.port;
+
+const server = http.createServer((req, res) => {});
---
+++ NodeEvents.mjs
@@ -0,0 +1,16 @@
+import { EventEmitter } from 'node:events';
+
+class MyEmitter extends EventEmitter { }
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('Its late Night!', () => {
+    console.log('Go To Sleep!');
+    setTimeout(() => {
+        console.log('Stop Working Please Go To Sleep Now!');
+    }, 3000);
+});
+
+console.log("The Script Is Running!");
+myEmitter.emit('Its late Night!');
+console.log("The Script Is Still Running!");
