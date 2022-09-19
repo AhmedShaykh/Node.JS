2fe63929677523ccdc4161ee910569caa7a796c0
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-19 15:50:51 +0500

    Git Init

--- Express.js
+++ Express.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Learning Express Node.JS!</h1>')
 })
 
+app.get('/about', (req, res) => {
+    res.send('<h1>This Is An About Page!</h1>')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
