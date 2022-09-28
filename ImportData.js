5c04f5f5189764ad1f4907f0c90d2524e464177b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-09-28 13:56:37 +0500

    Git Init

--- ImportData.js
+++ ImportData.js
@@ -1,12 +1,9 @@
-// (function (exports, require, module, __filename, __dirname) {
-// console.log(exports, require, module, __filename, __dirname);
+console.log(exports, require, module, __filename, __dirname);
 
-data = {
+var data = {
     name: "Node.JS",
     version: 16,
     frameWork: true,
 }
 
 module.exports = data;
-
-// })
